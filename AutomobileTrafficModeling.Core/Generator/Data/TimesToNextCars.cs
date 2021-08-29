3cd465a819fb195dc3c53fd811e2cb56c5cc3288
novagc <novagc@yandex.ru>
2021-08-29 23:24:16 +0900

    change times

--- AutomobileTrafficModeling.Core/Generator/Data/TimesToNextCars.cs
+++ AutomobileTrafficModeling.Core/Generator/Data/TimesToNextCars.cs
@@ -8,14 +8,15 @@
         public readonly short Right;
 
         /// <summary>
-        /// If the time is less than 0, then the cars will not appear
+        /// If the time is less than 0, then the cars will not appear <br/>
+        /// If the time is 0, then the cars will appear every turn
         /// </summary>
         public TimesToNextCars(short up = -1, short down = -1, short left = -1, short right = -1)
         {
-            Up = up;
-            Down = down;
-            Left = left;
-            Right = right;
+            Up = (short)(up + 1);
+            Down = (short)(down + 1);
+            Left = (short)(left + 1);
+            Right = (short)(right + 1);
         }
     }
 }
